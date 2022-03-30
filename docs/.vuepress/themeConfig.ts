fac54744e95b64e23690541210a1b4444b7902c0
FuckDoctors <zhbchwin@163.com>
2022-03-30 14:01:38 +0800

    feat(themeconfig): 修改themeConfig

--- docs/.vuepress/themeConfig.ts
+++ docs/.vuepress/themeConfig.ts
@@ -3,54 +3,27 @@ import * as navbar from './navbar'
 import * as sidebar from './sidebar'
 
 export default defineThemeConfig({
-  hostname: 'https://vuepress-theme-hope-v2-demo.mrhope.site',
+  hostname: 'https://zhaobc.site',
 
   author: {
     name: 'Mr.Hope',
-    url: 'https://mrhope.site',
+    url: 'https://zhaobc.site',
   },
 
   iconPrefix: 'iconfont icon-',
 
   logo: '/logo.svg',
 
-  repo: 'https://github.com/vuepress-theme-hope/vuepress-theme-hope',
+  repo: 'https://github.com/FuckDoctors/notes2',
 
-  docsDir: 'demo/src',
+  docsDir: 'docs',
 
   pageInfo: ['Author', 'Original', 'Date', 'Category', 'Tag', 'ReadingTime'],
 
   blog: {
     medias: {
-      Baidu: 'https://example.com',
-      Bitbucket: 'https://example.com',
-      Dingding: 'https://example.com',
-      Discord: 'https://example.com',
-      Dribbble: 'https://example.com',
-      Email: 'https://example.com',
-      Evernote: 'https://example.com',
-      Facebook: 'https://example.com',
-      Flipboard: 'https://example.com',
-      Gitee: 'https://example.com',
-      GitHub: 'https://example.com',
-      Gitlab: 'https://example.com',
-      Gmail: 'https://example.com',
-      Instagram: 'https://example.com',
-      Lines: 'https://example.com',
-      Linkedin: 'https://example.com',
-      Pinterest: 'https://example.com',
-      Pocket: 'https://example.com',
-      QQ: 'https://example.com',
-      Qzone: 'https://example.com',
-      Reddit: 'https://example.com',
-      Rss: 'https://example.com',
-      Steam: 'https://example.com',
-      Twitter: 'https://example.com',
-      Wechat: 'https://example.com',
-      Weibo: 'https://example.com',
-      Whatsapp: 'https://example.com',
-      Youtube: 'https://example.com',
-      Zhihu: 'https://example.com',
+      Email: 'mailto:zhbchwin@163.com',
+      GitHub: 'https://github.com/FuckDoctors',
     },
   },
 
@@ -108,10 +81,10 @@ export default defineThemeConfig({
     // you can also use Waline
     comment: {
       type: 'giscus',
-      repo: 'vuepress-theme-hope/giscus-discussions',
-      repoId: 'R_kgDOG_Pt2A',
+      repo: 'FuckDoctors/notes2',
+      repoId: 'R_kgDOHFsMkw',
       category: 'Announcements',
-      categoryId: 'DIC_kwDOG_Pt2M4COD69',
+      categoryId: 'DIC_kwDOHFsMk84COVMW',
     },
 
     mdEnhance: {
