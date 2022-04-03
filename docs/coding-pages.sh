0f02f825e06501c90b48b97c2634aff93070997b
FuckDoctors <zhbchwin@163.com>
2022-04-04 01:28:30 +0800

    ci: 修复known_hosts不存在的问题

--- docs/coding-pages.sh
+++ docs/coding-pages.sh
@@ -12,6 +12,7 @@ git add -A
 git commit -m "Deploying docs [ci skip] at $(TZ='Asia/Shanghai' date "+%Y-%m-%d %H:%M:%S %z")"
 
 # e.coding.net
+touch ~/.ssh/known_hosts
 ssh-keyscan -H e.coding.net >> ~/.ssh/known_hosts
 git push -f git@e.coding.net:fuckdoctors/notes.git master:master
 
