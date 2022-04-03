beb220d44d8eb0860114f48be8d785f3c4f40579
FuckDoctors <zhbchwin@163.com>
2022-04-03 23:56:49 +0800

    ci: 添加coding pages

---
+++ docs/coding-pages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+git config --global user.email "zhbchwin@163.com"
+git config --global user.name "ZhaoBin"
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m "Deploying docs [ci skip] at $(TZ='Asia/Shanghai' date "+%Y-%m-%d %H:%M:%S %z")"
+
+# e.coding.net
+ssh-keyscan -H e.coding.net >> ~/.ssh/known_hosts
+git push -f git@e.coding.net:fuckdoctors/notes.git master:master
+
+cd -
+rm -rf docs/.vuepress/dist
