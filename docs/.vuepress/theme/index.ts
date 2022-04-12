19209159a448aed5e256d0ac5cc8b1136f0bf9f5
FuckDoctors <zhbchwin@163.com>
2022-04-13 01:12:01 +0800

    feat(sidebar): 增加自定义sidebarText的功能
    主要用来自定义文件夹下README.md的侧边栏的文本

--- docs/.vuepress/theme/index.ts
+++ docs/.vuepress/theme/index.ts
@@ -1,8 +1,14 @@
 // 主题继承
 // https://v2.vuepress.vuejs.org/reference/default-theme/extending.html#extending
 // https://vuepress-theme-hope.github.io/v2/zh/cookbook/advanced/extend.html#%E7%BB%A7%E6%89%BF%E4%B8%BB%E9%A2%98
-import type { ThemeObject } from '@vuepress/core'
+import type { ThemeObject, App } from '@vuepress/core'
 import { path } from '@vuepress/utils'
+
+import { getThemeConfig } from 'vuepress-theme-hope/lib/node/themeConfig'
+import { prepareSidebarData } from './node/sidebar'
+import { prepareThemeColorScss } from 'vuepress-theme-hope/lib/node/themeColor'
+
+import themeOptions from '../themeConfig'
 
 const themeZhaobc: ThemeObject = {
   name: 'vuepress-theme-zhaobc',
@@ -17,6 +23,17 @@ const themeZhaobc: ThemeObject = {
   },
   layouts: {
     // 你可以在这里覆盖或新增布局
+  },
+
+  // 覆盖原来的onPrepared，使用自定义的prepareSidebarData
+  // 以便自定义sidebarText
+  onPrepared: (app: App): Promise<void> => {
+    const themeConfig = getThemeConfig(app, themeOptions)
+
+    return Promise.all([
+      prepareSidebarData(app, themeConfig),
+      prepareThemeColorScss(app, themeConfig),
+    ]).then(() => void 0)
   },
 }
 
