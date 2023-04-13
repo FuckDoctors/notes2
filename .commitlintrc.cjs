01cfddb2c1f89977673bdfd2670baef7140fc4ec
Zhao Bin <413853119@qq.com>
2023-04-13 22:59:40 +0800

    chore(linter): 更改commitlint配置

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -2,4 +2,8 @@
 module.exports = {
   // parserPreset: 'conventional-changelog-conventionalcommits',
   extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'subject-case': [1, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']]
+  }
 }
