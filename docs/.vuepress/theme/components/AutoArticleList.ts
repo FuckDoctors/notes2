a9fa769cef49959b230af80789eec7ac81f3ae5f
Zhao Bin <413853119@qq.com>
2023-04-17 18:00:39 +0800

    feat(articles): 自动生成文字列表

---
+++ docs/.vuepress/theme/components/AutoArticleList.ts
@@ -0,0 +1,51 @@
+import { type VNode, defineComponent, h, ref, computed } from 'vue'
+
+import BreadCrumb from '@theme-hope/components/BreadCrumb'
+import MarkdownContent from '@theme-hope/components/MarkdownContent'
+import DropTransition from '@theme-hope/components/transitions/DropTransition'
+import ArticleList from '@theme-hope/modules/blog/components/ArticleList'
+import { useArticles } from '@theme-hope/modules/blog/composables/index'
+
+import '../styles/auto-article-list.scss'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  name: 'AutoArticleList',
+
+  setup() {
+    const articles = useArticles()
+    const router = useRouter()
+
+    const currRoute = router.currentRoute
+
+    const currArticles = computed(() => {
+      return articles.value.items.filter(
+        item =>
+          item.path.startsWith(currRoute.value.path) &&
+          item.path !== currRoute.value.path
+      )
+    })
+
+    return (): VNode =>
+      h('div', { class: 'page blog' }, [
+        h('div', { class: 'blog-page-wrapper' }, [
+          h(
+            'main',
+            {
+              class: 'auto-article-list',
+              id: 'auto-article-list-main-content',
+            },
+            [
+              h(BreadCrumb),
+              h(DropTransition, { appear: true, delay: 0.24 }, () =>
+                h(ArticleList, { items: currArticles.value })
+              ),
+            ]
+          ),
+        ]),
+        h(DropTransition, { appear: true, delay: 0.28 }, () =>
+          h(MarkdownContent)
+        ),
+      ])
+  },
+})
---
+++ docs/posts/devops/README.md
@@ -0,0 +1,5 @@
+---
+layout: AutoArticleListLayout
+index: false
+article: false
+---
