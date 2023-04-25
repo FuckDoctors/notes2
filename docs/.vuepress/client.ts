6a7a53fda96261fb059c8dd47a2bc42bc638a29d
Zhao Bin <413853119@qq.com>
2023-04-25 18:33:34 +0800

    feat(presets): 开启特性闪光和图标跳到的预设

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -3,6 +3,11 @@ import { defineClientConfig } from '@vuepress/client'
 import AutoArticleListLayout from './theme/layouts/AutoArticleListLayout'
 import AutoArticleList from './theme/components/AutoArticleList'
 
+// 为项目主页的特性添加闪光效果
+import 'vuepress-theme-hope/presets/shinning-feature-panel.scss'
+// 为页面图标添加鼠标悬停的跳动效果
+import 'vuepress-theme-hope/presets/bounce-icon.scss'
+
 export default defineClientConfig({
   // You can override or add layouts here
   layouts: {
